4d6b557062a293d711326427aad8d2efe988ca77
jasnyder <jasnyder@plum.com>
2016-08-08 14:20:43 -0700

    checking javascript

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
-function alertFunc(){
-    alert("script from geoLocation is being read in...")
-}
+$(document).on('pageinit', function(){
+alert("script from geoLocation is being read in...")
+});
