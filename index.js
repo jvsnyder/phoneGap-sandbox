4be07e2d554efbb854c42edbde77fbcb101fa1be
jasnyder <jasnyder@plum.com>
2016-08-08 11:00:51 -0700

    checking javascript

--- index.js
+++ index.js
@@ -2,25 +2,5 @@ $latitude = null
 $longitude = null
 
 $(document).ready(function(){
-    document.addEventListener('deviceready', onDeviceReady, false)
-
-    function onDeviceReady() {
-        
-    }
-
-    function geo(pos)
-    {
-        $latitude = pos.coords.latitude
-        $longitude = pos.coords.$longitude
-    }
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(geo, function(er){alert('Unable to locate device')})
-        alert('got lat and long')
-    }
-    else {
-        alert('the application requires geolocation to be enabled on the device')
-    }
-
-
+    alert("script from geoLocation is being read in...")
 })
